56652f6016cb803fec9537bec623bfec2993a103
Worlor <worlor@hotmail.fr>
2016-01-12 13:49:03 +0100

    Creation de l'affichage d'une piste Fond colorié en gris

--- Game.js
+++ Game.js
@@ -53,6 +53,188 @@ var GF = function () {
         return (speed * delta) / 1000;
     };
 
+    // Player logic
+    function movePlayer(x, y) {
+        // draw a big player !
+        // head
+
+        // save the context
+        ctx.save();
+
+        // translate the coordinate system, draw relative to it
+        ctx.translate(x, y);
+        ctx.scale(0.5, 0.5);
+
+        // (0, 0) is the top left corner of the player.
+        ctx.strokeRect(0, 0, 100, 100);
+
+        // restore the context
+        ctx.restore();
+    }
+
+    function updatePlayerPosition(delta) {
+        player.speedX = 0;
+        // check inputStates
+        if (inputStates.left) {
+            player.speedX = -player.speed;
+        }
+        if (inputStates.right) {
+            player.speedX = player.speed;
+        }
+        /*if (inputStates.space) {
+         }
+         if (inputStates.mousePos) {
+         }
+         if (inputStates.mousedown) {
+         player.speed = 500;
+         } else {
+         // mouse up
+         player.speed = 100;
+         }*/
+
+        // collision avec obstacles
+        /*for(var i=0; i < obstacles.length; i++) {
+         var o = obstacles[i];
+         if(rectsOverlap(o.x, o.y, o.w, o.h,
+         player.x, player.y, player.width, player.height)) {
+         console.log("collision");
+         //player.x = 10;
+         //player.y = 10;
+         player.speed = 30;
+         }
+         }*/
+        // Compute the incX and inY in pixels depending
+        // on the time elasped since last redraw
+        player.x += calcDistanceToMove(delta, player.speedX);
+
+        //Empeche le joueur de sortir de l'écran
+        if(player.x < 0)
+        {
+            player.x = 0;
+        }
+        if(player.x > ( w - player.width))
+        {
+            player.x = w - player.width;
+        }
+    }
+
+
+    //Objects logic
+
+    function updateObstacles(delta) {
+        for (var i =0; i < obstacles.length; i++) {
+            var obstacle = obstacles[i];
+            obstacle.move(delta);
+            testObstacleSortie(obstacle,canvas);
+            //testCollisionObstacleMur(obstacle, canvas);
+            obstacle.draw();
+        }
+    }
+
+    function testCollisionObstacleMur(obstacle, canvas) {
+        if((obstacle.y + obstacle.h) > canvas.height) {
+            obstacle.y = canvas.height-obstacle.h;
+            obstacle.speedY = - obstacle.speedY;
+        }
+        if(obstacle.y < 0 )  {
+            obstacle.y = 0;
+            obstacle.speedY = - obstacle.speedY;
+        }
+        if((obstacle.x + obstacle.w) > canvas.width) {
+            obstacle.x = canvas.width -obstacle.w;
+            obstacle.speedX = - obstacle.speedX;
+        }
+        if(obstacle.x < 0) {
+            obstacle.x = 0;
+            obstacle.speedX = - obstacle.speedX;
+        }
+    }
+
+
+    // Collisions between aligned rectangles
+    function rectsOverlap(x1, y1, w1, h1, x2, y2, w2, h2) {
+
+        if ((x1 > (x2 + w2)) || ((x1 + w1) < x2))
+            return false; // No horizontal axis projection overlap
+        if ((y1 > (y2 + h2)) || ((y1 + h1) < y2))
+            return false; // No vertical axis projection overlap
+        return true;    // If previous tests failed, then both axis projections
+                        // overlap and the rectangles intersect
+    }
+
+    function creerObstacles() {
+        var obstacle1 = creerObstacle(Math.floor((Math.random() * w) + 1) );
+        obstacles.push(obstacle1);
+    }
+
+    function creerObstacle(x) {
+        var obstacle = new Obstacle(x, -1, 20, 200, 0, 150);
+        return obstacle;
+    }
+
+    function Obstacle(x, y, w, h, sx, sy) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.speedX = sx;
+        this.speedY = sy;
+        this.color = 'black';
+        this.launch = false;
+
+        this.draw = function () {
+            ctx.save();
+            ctx.fillRect(this.x, this.y, this.w, this.h);
+            ctx.restore();
+            this.color = 'black';
+        };
+
+
+        this.move = function (delta) {
+            // add horizontal increment to the x pos
+            // add vertical increment to the y pos
+
+            this.x += calcDistanceToMove(delta, this.speedX);
+            this.y += calcDistanceToMove(delta, this.speedY);
+        };
+    }
+
+    function creerPiste() {
+        for(var i = 0; i < h; i = i + 20) {
+            pisteG = new Piste(50, i);
+            pisteD = new Piste(300, i);
+            obstacles.push(pisteG, pisteD);
+        }
+    }
+
+    function Piste(x,y) {
+        this.x = x;
+        this.y = y;
+        this.w = 30;
+        this.h = 21;
+        this.speed = 100;
+
+        this.draw = function () {
+            ctx.save();
+            ctx.fillStyle="#FF0000";
+            ctx.fillRect(this.x, this.y, this.w, this.h / 2);
+            ctx.fillStyle="#FFFFFF";
+            ctx.fillRect(this.x, this.y + (this.h / 2), this.w, this.h / 2);
+        };
+
+        this.move = function (delta) {
+            this.y += calcDistanceToMove(delta, this.speed);
+        };
+
+        this.isOutOfScreen = function() {
+            if((this.y + this.h) > h ) {
+                return true;
+            }
+            return false;
+        }
+
+    }
+    // Game logic
     var measureFPS = function (newTime) {
 
         // test for the very first invocation
@@ -81,44 +263,19 @@ var GF = function () {
         ctx.clearRect(0, 0, w, h);
     }
 
-    // Functions for drawing the player and maybe other objects
-    function movePlayer(x, y) {
-        // draw a big player !
-        // head
-
-        // save the context
-        ctx.save();
-
-        // translate the coordinate system, draw relative to it
-        ctx.translate(x, y);
-        ctx.scale(0.5, 0.5);
-
-        // (0, 0) is the top left corner of the player.
-        ctx.strokeRect(0, 0, 100, 100);
-
-        // eyes
-        ctx.fillRect(20, 20, 10, 10);
-        ctx.fillRect(65, 20, 10, 10);
-
-        // nose
-        ctx.strokeRect(45, 40, 10, 40);
-
-        // mouth
-        ctx.strokeRect(35, 84, 30, 10);
-
-        // teeth
-        ctx.fillRect(38, 84, 10, 10);
-        ctx.fillRect(52, 84, 10, 10);
-
-        // restore the context
-        ctx.restore();
-    }
-
     function timer(currentTime) {
         var delta = currentTime - oldTime;
         oldTime = currentTime;
         return delta;
 
+    }
+    function getMousePos(evt) {
+        // necessary to take into account CSS boudaries
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
     }
     var mainLoop = function (time) {
         //main function, called each frame
@@ -135,88 +292,47 @@ var GF = function () {
         }
 
         switch (currentGameState) {
-        case gameStates.gameRunning:
+            case gameStates.gameRunning:
 
-            // draw the player
-            movePlayer(player.x, player.y);
+                // draw the player
+                movePlayer(player.x, player.y);
 
-            // Check inputs and move the player
-            updatePlayerPosition(delta);
+                // Check inputs and move the player
+                updatePlayerPosition(delta);
 
-            // update and draw balls
-            //updateBalls(delta);
-            updateObstacles(delta);
+                // update and draw balls
+                //updateBalls(delta);
+                updateObstacles(delta);
 
-            // display Score
-            displayScore();
+                // display Score
+                displayScore();
 
-            // decrease currentLevelTime.
-            // When < 0 go to next level
-            currentLevelTime += delta;
+                // decrease currentLevelTime.
+                // When < 0 go to next level
+                currentLevelTime += delta;
 
-            /* if (currentLevelTime < 0) {
-             goToNextLevel();
-             }*/
+                /* if (currentLevelTime < 0) {
+                 goToNextLevel();
+                 }*/
 
-            break;
-        case gameStates.mainMenu:
-            // TO DO !
-            break;
-        case gameStates.gameOver:
-            ctx.fillText("GAME OVER", 50, 100);
-            ctx.fillText("Press SPACE to start again", 50, 150);
-            ctx.fillText("Move with arrow keys", 50, 200);
-            ctx.fillText("Survive 5 seconds for next level", 50, 250);
-            if (inputStates.space) {
-                this.startNewGame();
-            }
-            break;
+                break;
+            case gameStates.mainMenu:
+                // TO DO !
+                break;
+            case gameStates.gameOver:
+                ctx.fillText("GAME OVER", 50, 100);
+                ctx.fillText("Press SPACE to start again", 50, 150);
+                ctx.fillText("Move with arrow keys", 50, 200);
+                ctx.fillText("Survive 5 seconds for next level", 50, 250);
+                if (inputStates.space) {
+                    this.startNewGame();
+                }
+                break;
         }
 
         // call the animation loop every 1/60th of second
         requestAnimationFrame(mainLoop);
     };
-
-    function updateObstacles(delta) {
-        for (var i =0; i < obstacles.length; i++) {
-            var obstacle = obstacles[i];
-            obstacle.move(delta);
-            testObstacleSortie(obstacle,canvas);
-            //testCollisionObstacleMur(obstacle, canvas);
-            obstacle.draw();
-        }
-    }
-
-    function testObstacleSortie(obstacle, canvas)
-    {
-        if(obstacle.launch == false) {
-            return false;
-        }
-
-        if(obstacle.y > canvas.height) {
-            return true;
-        }
-
-    }
-
-    function testCollisionObstacleMur(obstacle, canvas) {
-        if((obstacle.y + obstacle.h) > canvas.height) {
-            obstacle.y = canvas.height-obstacle.h;
-            obstacle.speedY = - obstacle.speedY;
-        }
-        if(obstacle.y < 0 )  {
-            obstacle.y = 0;
-            obstacle.speedY = - obstacle.speedY;
-        }
-        if((obstacle.x + obstacle.w) > canvas.width) {
-            obstacle.x = canvas.width -obstacle.w;
-            obstacle.speedX = - obstacle.speedX;
-        }
-        if(obstacle.x < 0) {
-            obstacle.x = 0;
-            obstacle.speedX = - obstacle.speedX;
-        }
-    }
 
     function startNewGame() {
         player.dead = false;
@@ -239,110 +355,6 @@ var GF = function () {
         ctx.fillText("Time: " + (currentLevelTime / 1000).toFixed(1), 300, 60);
         ctx.restore();
     }
-    function updatePlayerPosition(delta) {
-        player.speedX = 0;
-        // check inputStates
-        if (inputStates.left) {
-            player.speedX = -player.speed;
-        }
-        if (inputStates.right) {
-            player.speedX = player.speed;
-        }
-        /*if (inputStates.space) {
-        }
-        if (inputStates.mousePos) {
-        }
-        if (inputStates.mousedown) {
-            player.speed = 500;
-        } else {
-            // mouse up
-            player.speed = 100;
-        }*/
-
-        // collision avec obstacles
-        /*for(var i=0; i < obstacles.length; i++) {
-            var o = obstacles[i];
-            if(rectsOverlap(o.x, o.y, o.w, o.h,
-                    player.x, player.y, player.width, player.height)) {
-                console.log("collision");
-                //player.x = 10;
-                //player.y = 10;
-                player.speed = 30;
-            }
-        }*/
-        // Compute the incX and inY in pixels depending
-        // on the time elasped since last redraw
-        player.x += calcDistanceToMove(delta, player.speedX);
-
-        //Empeche le joueur de sortir de l'écran
-        if(player.x < 0)
-        {
-            player.x = 0;
-        }
-        if(player.x > ( w - player.width))
-        {
-            player.x = w - player.width;
-        }
-    }
-
-
-
-    // Collisions between aligned rectangles
-    function rectsOverlap(x1, y1, w1, h1, x2, y2, w2, h2) {
-
-        if ((x1 > (x2 + w2)) || ((x1 + w1) < x2))
-            return false; // No horizontal axis projection overlap
-        if ((y1 > (y2 + h2)) || ((y1 + h1) < y2))
-            return false; // No vertical axis projection overlap
-        return true;    // If previous tests failed, then both axis projections
-                        // overlap and the rectangles intersect
-    }
-
-    function getMousePos(evt) {
-        // necessary to take into account CSS boudaries
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        };
-    }
-
-    function creerObstacles() {
-        var obstacle1 = creerObstacle(Math.floor((Math.random() * w) + 1) );
-        obstacles.push(obstacle1);
-    }
-
-    function creerObstacle(x) {
-        var obstacle = new Obstacle(x, -1, 20, 200, 0, 150);
-        return obstacle;
-    }
-
-// constructor function for balls
-    function Obstacle(x, y, w, h, sx, sy) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.speedX = sx;
-        this.speedY = sy;
-        this.color = 'black';
-        this.launch = false;
-
-        this.draw = function () {
-            ctx.save();
-            ctx.fillRect(this.x, this.y, this.w, this.h);
-            ctx.restore();
-            this.color = 'black';
-        };
-
-        this.move = function (delta) {
-            // add horizontal increment to the x pos
-            // add vertical increment to the y pos
-
-            this.x += calcDistanceToMove(delta, this.speedX);
-            this.y += calcDistanceToMove(delta, this.speedY);
-        };
-    }
 
     function loadAssets(callback) {
         // here we should load the souds, the sprite sheets etc.
@@ -361,6 +373,8 @@ var GF = function () {
             }
         });
     }
+
+
     var start = function () {
         // adds a div for displaying the fps value
         fpsContainer = document.createElement('div');
@@ -425,7 +439,8 @@ var GF = function () {
         player.x = (w / 2) - (player.width / 2)
         player.y = h - player.height;
 
-        creerObstacles();
+        //creerObstacles();
+        creerPiste();
 
         // all assets (images, sounds) loaded, we can start the animation
         requestAnimationFrame(mainLoop);
